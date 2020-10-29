097ba8d0b9269e690a2f26785b05266dbb150de6
namanseth01 <naman.seth_cs19@gla.ac.in>
2020-10-29 16:08:50 +0530

    override the run method in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -2,14 +2,14 @@ package thread.tortoise;
 
 public class Tortoise extends Thread {
     private static final int MILESTONES = 5;
-    private final Thread tortoise;
 
     public Tortoise() {
-        this.tortoise = new Thread(this, "tortoise");
+        super("tortoise");
     }
 
-    public Thread getTortoise() {
-        return tortoise;
+    @Override
+    public void run() {
+
     }
 }
 
