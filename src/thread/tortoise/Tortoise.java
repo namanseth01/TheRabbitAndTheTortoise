eccca0825636a6b5636e1e2d4a3767ec6910ff8b
namanseth01 <naman.seth_cs19@gla.ac.in>
2020-10-29 15:42:43 +0530

    add the constructor  in Tortoise.java class

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -2,4 +2,15 @@ package thread.tortoise;
 
 public class Tortoise extends Thread {
     private static final int MILESTONES = 5;
+    private final Thread tortoise;
+
+    public Tortoise() {
+        this.tortoise = new Thread(this, "tortoise");
+    }
+
+    public Thread getTortoise() {
+        return tortoise;
+    }
 }
+
+
