a6e31f7b93d975f5def6989d227140b44b7b24ea
namanseth01 <naman.seth_cs19@gla.ac.in>
2020-10-29 16:11:30 +0530

    add the body for the overridden method

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -9,7 +9,30 @@ public class Tortoise extends Thread {
 
     @Override
     public void run() {
-
+        for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The Tortoise has started from the start line!");
+                    break;
+                case 1:
+                    System.out.println("The Tortoise has reached the Cool River!");
+                    break;
+                case 2:
+                    System.out.println("The Tortoise has reached the Mountain Hill!");
+                    break;
+                case 3:
+                    System.out.println("The Tortoise has reached the Big Oak Tree!");
+                    break;
+                case 4:
+                    System.out.println("The Tortoise has reached the finish line!");
+                    break;
+            }
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {
+                System.err.println("The Tortoise has won the path!");
+            }
+        }
     }
 }
 
